ca7b2d481c8ebba704e8931a9ba00493947b2d88
amaged1896 <ahmed.maged1896@gmail.com>
2023-10-25 00:04:15 +0300

    update"

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connection = async () => {
-    await mongoose.connect('mongodb://127.0.0.1:27017/Anyware-Software-Task')
+    await mongoose.connect(process.env.DATABASE_LOCAL)
         .then(() => console.log("Database connection established"))
         .catch(err => console.log(`Database connection error: ${err}`));
 };
