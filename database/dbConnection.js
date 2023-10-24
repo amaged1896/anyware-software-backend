16d444282081e53f1ba703ead1ae27c0905352b2
amaged1896 <ahmed.maged1896@gmail.com>
2023-10-25 01:14:42 +0300

    "add atlas"

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connection = async () => {
-    await mongoose.connect(process.env.DATABASE_LOCAL)
+    await mongoose.connect(process.env.DATABASE_ATLAS)
         .then(() => console.log("Database connection established"))
         .catch(err => console.log(`Database connection error: ${err}`));
 };
