ddc4bd196ed49793bad9a2af1bcfcf3b3247d3ef
amaged1896 <ahmed.maged1896@gmail.com>
2023-10-25 18:28:07 +0300

    "update quiz modell"

--- database/models/quiz.model.js
+++ database/models/quiz.model.js
@@ -18,9 +18,6 @@ const quizSchema = mongoose.Schema({
         transform: function (doc, ret) {
             // Modify the serialization of the 'dueTo' field
             ret.dueTo = ret.dueTo.toISOString().split('T')[0];
-            // Optionally, you can delete the '_id' and '__v' fields if you don't want them in the response
-            delete ret._id;
-            delete ret.__v;
         }
     }
 });
